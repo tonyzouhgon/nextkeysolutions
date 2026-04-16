ee666c7f5b01403a5162658ef972e054adb4c062
nextkeysolutions <nextkeysolutionss@gmail.com>
2026-04-15 23:47:33 -0700

    app.js

--- app.js
+++ app.js
@@ -394,3 +394,14 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+document.querySelectorAll(".reveal").forEach(el => {
+  observer.observe(el);
+});
